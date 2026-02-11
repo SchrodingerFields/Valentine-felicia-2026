1b6048f74f8e630689c960b5f61a7444a2e14260
bottlesauce <vaudiganteng123@gmail.com>
2026-02-11 20:04:00 +0700

    web-valentine-felicia-revisi

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  base: "/Valentine-felicia-2026/",
+  plugins: [react()], // Kita hapus bagian babel-compiler yang bikin error
+})  
